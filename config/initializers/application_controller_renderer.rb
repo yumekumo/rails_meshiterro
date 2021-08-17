8491c086b0cd8a7e91a89a46d3382be7b8315d73
yumekumo <chihachiha.ryotakun@gmail.com>
2021-08-17 02:36:15 +0000

    [Finish] 4-3]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'bd5ef3b55c4161aa6cfe0359d87f4cc057b8ca07580fc3e452f59e1eba3edd42c75bb647085e8e9641e61bfaa77c3dc9937b74e4c7a7410cea20d50b55f26183'
