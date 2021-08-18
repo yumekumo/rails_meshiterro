beb5df86f674d691e5619b7667b8c1a4c6d3c6d4
yumekumo <chihachiha.ryotakun@gmail.com>
2021-08-18 14:59:41 +0000

    [Finish] 4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
@@ -6,5 +7,6 @@ Rails.application.routes.draw do
     
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
 
 end
